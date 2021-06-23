5a2b6515608f68e1fdd09edf609651c99b3a5527
ironex <darkwithachanceofpain@gmail.com>
2021-06-23 17:54:12 +0200

    Fix command description

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -6,10 +6,10 @@ docker container rm $(docker container ls -aq)
 
 docker image rm $(docker image ls -aq) --force
 
-echo "###### Docker container ls -a ######"
+echo "###### docker container ls -a"
 
 docker container ls -a
 
-echo "###### Docker image ls ######"
+echo "###### docker image ls"
 
 docker image ls
